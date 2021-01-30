781c77ac841292b4ee10cb5e6fe35da3f275ff30
Tatiane Mendes Camillo <tatiane@Alexs-MacBook-Pro.local>
2021-01-30 14:00:17 -0300

    eslint add and setState to name

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import QuizBackground from '../src/components/QuizBackground'
-import GithubCorner from '../src/components/GithubCorner'
+import styled from 'styled-components';
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const Title = styled.h1 ` 
-  font-size: 50px;
-  color: ${ ({ theme }) => theme.colors.primary };
-`
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import QuizBackground from '../src/components/QuizBackground';
+import GithubCorner from '../src/components/GithubCorner';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -22,28 +21,50 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName ] = React.useState('');
+  console.log(name, setName);
+
   return (
-      <QuizBackground backgroundImage={db.bg}>
-        <QuizContainer>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Head>
+          <title>Alura Quiz - Modelo Base</title>
+        </Head>
 
-            <Widget>
-              <Widget.Header>
-                <h1>The legend of Coffee</h1>
-              </Widget.Header>
-              <Widget.Content>
-                <p>lorem ipsum lorem ipsum...</p>
-              </Widget.Content>
-            </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of Coffee</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // console.log('fazendo uma submissao');
+            }}
+            >
+              <input
+                placeholder="Digite seu nome"
+                // eslint-disable-next-line func-names
+                onChange={function (e) {
+                  // eslint-disable-next-line no-unused-expressions
+                  setName(e.target.value);
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}> Jogar</button>
+            </form>
+          </Widget.Content>
+        </Widget>
 
-            <Widget>
-              <Widget.Content>
-                  <h1>The legend of Coffee</h1>
-                  <p>lorem ipsum lorem ipsum...</p>
-                </Widget.Content>
-              </Widget>
-            <Footer />
-        </QuizContainer>
-        <GithubCorner projectUrl="https://github.com/omariosauto"/>
-      </QuizBackground>
-  )
+        <Widget>
+          <Widget.Content>
+            <h1>The legend of Coffee</h1>
+            <p>lorem ipsum lorem ipsum...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/omariosauto" />
+    </QuizBackground>
+  );
 }
--- src/components/GithubCorner/index.js
+++ src/components/GithubCorner/index.js
@@ -44,4 +44,4 @@ export default function GithubCorner({ projectUrl }) {
       </a>
     </Wrapper>
   );
-} 
+}
